a039e19b8f4691d2e1d81225dcbf96480aa15298
prkarjgi <pkarajgikar@gmail.com>
2020-09-13 11:21:46 +0530

    First Unit Test added for lists, dummy view created, URL mapped to home

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
-# Create your tests here.
-class SmokeTest(TestCase):
-    # expected failure testcase
-    def test_sample(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolve_home_page(self):
+        homepage = resolve('/')
+        self.assertEqual(homepage.func, home_page)
