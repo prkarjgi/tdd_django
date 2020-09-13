5e1ea5ed3663147f0cdceea0c35a7a4c6ab7a642
prkarjgi <pkarajgikar@gmail.com>
2020-09-13 14:16:41 +0530

    Refactored homepage tests, view function and added lists to app settings. Added templates to lists.

--- lists/tests.py
+++ lists/tests.py
@@ -5,15 +5,6 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolve_home_page(self):
-        homepage = resolve('/')
-        self.assertEqual(homepage.func, home_page)
-
-    def test_root_url_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+    def test_home_page_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
