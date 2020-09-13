5cf8bea83caec42d8bf30659cb9cfdbabcdf8a63
prkarjgi <pkarajgikar@gmail.com>
2020-09-13 12:49:26 +0530

    Updated new visitor FT: Checking header, Entering new item, Checking if new item added.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,15 +22,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # User sees To-Do in the title of the page and sees To-Do lists in the header
         self.assertIn('To-Do', self.browser.title)
-        self.fail('TO-DO: COMPLETE THE REST OF THIS TEST')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-do', header_text)
+
+        # The user sees an input box telling them to enter a new item to the list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            "enter a new item",
+            inputbox.get_attribute("placeholder")
+        )
 
         # The user enters an item in their to-do list
-
+        inputbox.send_keys("Buy 3 milk bags")
         # They type the item in the text box and hit enter and the page updates and
         # adds a new item in their to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # They see the new item added to the to-do list
+        to_do_items = self.browser.find_element_by_id("id_to_do_list")
+        rows = to_do_items.find_elements_by_tag_name("tr")
+        self.assertIn(
+            "Buy 3 milk bags",
+            rows
+        )
 
         # The text box is still there and the user can enter another item. They add
         # another item and hit enter
+        self.fail("ADD TESTS FOR SUBSEQUENT ENTRIES AND REMEMBERING THE USER ACTIVITY")
 
         # There are now two new items on the to-do list
 
