6127696fe4d41e7cedb11f80600fcab8c9d85e68
prkarjgi <pkarajgikar@gmail.com>
2020-09-13 11:53:23 +0530

    unit test for home_page view added, home page view returns minimal html

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.http import HttpRequest
 from django.urls import resolve
+from django.test import TestCase
 from lists.views import home_page
 
 
@@ -7,3 +8,12 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_home_page(self):
         homepage = resolve('/')
         self.assertEqual(homepage.func, home_page)
+
+    def test_root_url_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.endswith("</html>"))
