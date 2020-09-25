e7f6939fd352c395bf8a669aa2f2dc4b07da3a18
prkarjgi <pkarajgikar@gmail.com>
2020-09-25 11:16:54 +0530

    Chapter 12: Added wait_for helper function to base test, completed FT for list item validation.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -17,14 +17,39 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_item(self):
         # User 1 goes to the home page
         # They accidentally hits enter without writing anything in the list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys(Keys.ENTER)
 
         # they sees that the page refreshes and is still on the home page
         # there is an error message saying the item cannot be left blank
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector(".has-error").text,
+                "You cannot add an empty list item"
+            )
+        )
 
         # They enter some values into the input box and hit enter
         # it adds to a new list
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Placeholder values for item")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_new_item_in_todo("1: Placeholder values for item")
 
         # as a second test, they hit enter again without entering an item
         # it refreshes and returns the same error message
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector(".has-error").text,
+                "You cannot add an empty list item"
+            )
+        )
 
-        self.fail("test has to be written")
+        # They add a new item and then check if the items are there in the list
+        self.browser.find_element_by_id("id_new_item").send_keys("dummy 2")
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
+
+        self.wait_for_new_item_in_todo("2: dummy 2")
+        self.wait_for_new_item_in_todo("1: Placeholder values for item")
